39bad24f3afa826de038fa366d476c61b38d0e2c
MainaZaquir <mzaquir58@gmail.com>
2023-11-11 18:08:46 +0300

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import TransactionTable from './TransactionTable';
+import TransactionForm from './TransactionForm';
+import NavBar from './Navbar';
 
 function App() {
+  const [transactions, setTransactions] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    fetch(' http://localhost:3000/transactions')
+      .then((Response) => Response.json())
+      .then((data) => setTransactions(data));
+  }, []);
+
+  const addTransaction = (transaction) => {
+    const maxId = Math.max(...transactions.map(t => t.id));
+    transaction.id = maxId + 1;
+    
+    setTransactions([...transactions, transaction]);
+  };
+
+  const deleteTransaction = (id) => {
+    setTransactions(transactions.filter((transaction) => transaction.id !== id));
+  };
+
+  const filteredTransactions = transactions.fiter((transaction) =>
+    transaction.description.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar setSearchTerm={setSearchTerm} />
+      <TransactionForm addTransaction={addTransaction} />
+      <TransactionTable
+        transactions={filteredTransactions}
+        deleteTransaction={deleteTransaction}
+      />
     </div>
   );
 }
