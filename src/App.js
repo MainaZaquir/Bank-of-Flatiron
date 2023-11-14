dc3b75c6641b9a3e4882f6fe3ad2436d6c4bc11a
MainaZaquir <mzaquir58@gmail.com>
2023-11-14 13:05:17 +0300

    Updated fetch code

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
-    fetch('http://localhost:3000/transactions')
+    fetch('https://my-json-server.typicode.com/MainaZaquir/Bank-of-Flatiron/transactions')
       .then((Response) => Response.json())
       .then((data) => {
         console.log(data);
